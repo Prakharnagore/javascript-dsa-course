556e11717e033b861535a0ba6207e3ca3db490f8
Prakharnagore000 <prakhar@kloudone.com>
2022-08-15 11:14:15 +0530

    Performance of Arrays and Objects

---
+++ 2. Performance of Arrays And Objects/performance.js
@@ -0,0 +1,47 @@
+// BUILT-IN DATA STRUCTURES
+// Through the lens of Big O
+
+// Objects
+// Unordered key value pairs
+// When to use objects
+// When you don't need order
+// When you need fast access / insertion and removal
+
+// Big O of Objects
+// Insertion -   O(1)
+// Removal -   O(1)
+// Searching -   O(N)
+// Access -   O(1)
+
+// Big O of Object Methods
+// Object.keys -   O(N)
+// Object.values -   O(N)
+// Object.entries -   O(N)
+// hasOwnProperty -   O(1)
+
+// Arrays
+// Ordered lists!
+// WHEN TO USE ARRAYS
+// When you need order
+// When you need fast access / insertion and removal (sort of....)
+
+// Big O of Arrays
+// Insertion -   It depends....
+// Removal -   It depends....
+// Searching -   O(N)
+// Access -   O(1)
+
+// Big O of Array Operations
+// push -   O(1)
+// pop -   O(1)
+// shift -   O(N)
+// unshift -   O(N)
+// concat -   O(N)
+// slice -   O(N)
+// splice -   O(N)
+// sort -   O(N * log N)
+// forEach/map/filter/reduce/etc. -   O(N)
+
+// Limitations of Arrays
+// Inserting at the beginning is not as easy as we might think!
+// There are more efficient data structures for that!
