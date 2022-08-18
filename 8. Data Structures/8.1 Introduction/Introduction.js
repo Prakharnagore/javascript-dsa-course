645b644202b47740fcc6717efacfbc2731529bc0
Prakharnagore000 <prakhar@kloudone.com>
2022-08-18 14:36:13 +0530

    ds intro

---
+++ 8. Data Structures/8.1 Introduction/Introduction.js
@@ -0,0 +1,21 @@
+// Data structures are collections of values, the relationships among them,
+// and the functions or operations that can be applied to the data
+
+// They all (data structures) excel in different situations .
+// Working with map/location data? - Use a graph!
+// Need an ordered list with fast inserts/removals at the beginning and end? - Use a linked list!
+// Web scraping nested HTML? -  Use a tree!
+// Need to write a scheduler? - Use a binary heap!
+
+// What is a class?
+// A blueprint for creating objects with pre-defined properties and methods
+
+// THE SYNTAX
+// The method to create new objects must be called constructor
+// The class keyword creates a constant, so you can not redefine it. Watch out for the syntax as well!
+
+// Classes are blueprints that when created make objects known as instances
+// Classes are created with the new keyword
+// The constructor function is a special function that gets run when the class is instantiated
+// Instance methods can be added to classes similar to methods in objects
+// Class methods can be added using the static keyword
