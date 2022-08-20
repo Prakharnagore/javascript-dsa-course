f54f8018d207d23318f7137745bc76acd6c368a9
prakharnagore <prakharnagore111@gmail.com>
2022-08-20 23:52:13 +0530

    hashtables

---
+++ 8. Data Structures/8.6 HashTables/hashtable.js
@@ -0,0 +1,79 @@
+// Hash tables are used to store key-value pairs.
+// Hash tables store data in a large array, and work by hashing the keys
+// Separate chaining and linear probing are two strategies used to deal with two keys that hash to the same index
+// They are like arrays, but the keys are not ordered.
+// Unlike arrays, hash tables are fast for all of the following operations: finding values, adding new values, and removing values!
+// colors["cyan"] is way better than colors[2]
+/*
+BIG O of HASH TABLES
+Insert: O(1)
+Deletion: O(1)
+Access: O(1)
+*/
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
+}
+
+const hash = new HashTable();
+hash.set("black", "#000");
+hash.set("white", "#fff");
+hash.get("white");
+hash.keys();
+hash.values();
